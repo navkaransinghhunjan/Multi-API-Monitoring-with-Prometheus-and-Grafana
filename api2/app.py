456c546f6c781006bb2f365505feb09d2809c2e4
Navkaran Singh <navkaransinghhunjan@gmail.com>
2024-11-06 17:53:06 +0530

    Added files

---
+++ api2/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+import time
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)  # Exposes /metrics endpoint automatically
+
+@app.route("/")
+def home():
+    return "Welcome to API 2"
+
+@app.route("/status")
+def status():
+    return jsonify(status="up", timestamp=time.time())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
